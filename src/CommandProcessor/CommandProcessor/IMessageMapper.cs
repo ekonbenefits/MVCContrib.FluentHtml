8de7a5ffb80ddd97ff39e612eefc12d9355785d5
Jeffrey Palermo <jeffrey@clear-measure.com>
2014-11-06 12:08:40 -0800

    - cleaned up dead folders
    --HG--
    branch : feature/MVC 6 Reset

--- src/CommandProcessor/CommandProcessor/IMessageMapper.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MvcContrib.CommandProcessor
-{
-	public interface IMessageMapper
-	{
-		object MapUiMessageToCommandMessage(object message, Type messageType, Type destinationType);
-	}
-}
--- src/MvcContrib.TestHelper.WatiN/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("UITestHelper.WatiN")]
