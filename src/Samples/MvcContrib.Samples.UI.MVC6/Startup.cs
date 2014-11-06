28319c54fd9db97edaf0e62954289606e96ed79f
Brendan Enrick <Brendan@Clear-Measure.com>
2014-11-06 21:01:38 +0000

    Adding a new MVC6 project
    --HG--
    branch : feature/MVC 6 Reset

---
+++ src/Samples/MvcContrib.Samples.UI.MVC6/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace MvcContrib.Samples.UI.MVC6
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
